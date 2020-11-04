272332f76889a753c2594f403ec5ba6067178189
Apurv singh <apurvsingh9713@gmail.com>
2020-11-04 21:59:42 +0530

    Initial commit

---
+++ Assets/Script/WinCondition.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinCondition : Movement
+{
+    [SerializeField] Transform leftTransform;
+    [SerializeField] Transform rightTransform;
+    [SerializeField] GameObject winPanel;
+
+    protected override void Start() {
+        base.Start();
+        
+        winPanel.SetActive(false);    
+    }
+    private void Update() {
+        if ((ball.transform.position.x - leftTransform.position.x) <= 0)
+        {
+            winPanel.SetActive(true);
+        }
+        if ((ball.transform.position.x - rightTransform.position.x) >= 0) 
+        {
+            winPanel.SetActive(true);
+        }
+    }
+
+    public void OnWin()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
